57312555bb75d815b34177706faa88a13fe3b567
Vinicius Oliveira Rocha <vinicius@Mac-mini-de-Stefany.local>
2023-01-31 18:57:16 -0300

    create adressform components

---
+++ src/pages/CheckoutOrder/components/AdressForm/index.tsx
@@ -0,0 +1,30 @@
+export function AdressForm() {
+  return (
+    <div>
+      <form action="">
+        <div>
+          <header>
+            <p>Endereço de entrega</p>
+            <p>informe o endereço de entrega</p>
+          </header>
+
+          <input type="text" name="" id="" />
+          <input type="text" name="" id="" />
+          <input type="number" name="" id="" />
+          <input type="number" name="" id="" />
+          <input type="number" name="" id="" />
+          <input type="number" name="" id="" />
+        </div>
+
+        <div>
+          <p>Pagamento</p>
+          <p>O pagamento é feito na entrega. Escolha a forma que deseja pagar</p>
+
+          <button>Cartao de credito</button>
+          <button>Cartao de debito</button>
+          <button>Dinheiro</button>
+        </div>
+      </form>
+    </div>
+  );
+}
